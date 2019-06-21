0a9a511a6bd6846adbd62420040ddc10a0082327
austin <austin@concentricsky.com>
2019-06-21 15:31:39 -0700

    add ngx-toastr dependency

--- packages/jdx-reference-application-ui/src/app/app.module.ts
+++ packages/jdx-reference-application-ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -8,6 +10,7 @@ import { ApiModule, Configuration } from '@jdx/jdx-reference-application-api-cli
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { MockBackendInterceptor } from './shared/mock/mock-backend-interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export function apiConfigFactory() {
   const params = {
@@ -26,12 +29,14 @@ export function apiConfigFactory() {
   ],
   imports: [
     ApiModule.forRoot(apiConfigFactory),
+    BrowserAnimationsModule,
     BrowserModule,
     // make sure to import the HttpClientModule in the AppModule only,
     // see https://github.com/angular/angular/issues/20575
     HttpClientModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     // { provide: HTTP_INTERCEPTORS, useClass: MockBackendInterceptor, multi: true }
