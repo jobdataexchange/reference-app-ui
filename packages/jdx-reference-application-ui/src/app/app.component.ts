86b36a08d614c672881ee03aa344bfba501f03fb
austin <austin@concentricsky.com>
2019-07-09 13:54:58 -0700

    add intercom

--- packages/jdx-reference-application-ui/src/app/app.component.ts
+++ packages/jdx-reference-application-ui/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Intercom } from 'ng-intercom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jdx-reference-application';
+
+  constructor(public intercom: Intercom) {}
+
+  ngOnInit(): void {
+    this.intercom.boot({});
+  }
 }
